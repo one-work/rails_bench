cf2b31c6cb7c0d4ca0d9829a5c0aaff6137996f3
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-19 23:47:26 +0800

    index

--- app/controllers/bench/board/organs_controller.rb
+++ app/controllers/bench/board/organs_controller.rb
@@ -3,6 +3,14 @@ module Bench
     before_action :set_provide
     before_action :set_organ, only: [:show, :edit, :update, :destroy, :actions, :bind]
 
+    def index
+      q_params = {}
+      #q_params.merge! provider_id: [current_organ.id, nil] if current_organ
+      q_params.merge! 'who_roles.role_id' => params[:role_id] if params[:role_id].present?
+
+      @members = current_user.members.includes(:organ).default_where(q_params)
+    end
+
     def create
       if @organ.save
         @provide.update(provider_id: @organ.id)
