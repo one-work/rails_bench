b7d5d5d58eae9bf89132b5089727a33ff774fff4
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-20 01:14:37 +0800

    faciltie provide

--- app/models/bench/model/facilitate_provide.rb
+++ app/models/bench/model/facilitate_provide.rb
@@ -15,7 +15,7 @@ module Bench
       belongs_to :provide, counter_cache: true
 
       has_many :facilitatings, primary_key: [:facilitate_id, :provide_id], foreign_key: [:facilitate_id, :provide_id]
-      has_many :facilitators, primary_key: [:facilitate_id, :provider_id], foreign_key: [:facilitate_id, :organ_id]
+      has_many :facilitators, primary_key: [:facilitate_id, :provider_id], foreign_key: [:facilitate_id, :organ_id], inverse_of: :facilitate_provide
       has_many :task_templates, as: :tasking, dependent: :nullify
     end
 
