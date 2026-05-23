9683fa17129a2188700c18674439a877cfc110e4
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-23 10:28:10 +0800

    fix

--- app/controllers/bench/facilitate_resells_controller.rb
+++ app/controllers/bench/facilitate_resells_controller.rb
@@ -7,7 +7,7 @@ module Bench
 
     def index
       q_params = {}
-      q_params.merge! default_params
+      q_params.merge! seller_id: current_organ.id
       q_params.merge! params.permit(:facilitate_taxon_id)
 
       @facilitates = Facilitate.default_where(q_params).page(params[:page])
---
+++ app/models/facilitate_resell.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+FacilitateResell = Bench::FacilitateResell unless defined? FacilitateResell
