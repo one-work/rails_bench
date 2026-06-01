2600828da375952ed108d239296486972815806a
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-01 10:55:29 +0800

    fix

--- app/models/bench/model/facilitate_resell.rb
+++ app/models/bench/model/facilitate_resell.rb
@@ -16,11 +16,16 @@ module Bench
       has_many :facilitatings, primary_key: [:facilitate_id, :provide_id], foreign_key: [:facilitate_id, :provide_id]
 
       before_save :sync_seller, if: -> { provide_id_changed? }
+      before_save :sync_from_facilitate, if: -> { faciliate_id_changed? }
       after_save :init_logo, if: -> { saved_change_to_facilitate_id? && facilitate }
     end
 
     def sync_seller
       self.seller = provide.provider
+    end
+
+    def sync_from_facilitate
+      self.name = facilitate.name
     end
 
     def init_logo
