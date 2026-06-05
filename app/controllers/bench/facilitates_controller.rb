1b530b31e26c4797e607c58377b57d9813866e0f
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-06 00:59:22 +0800

    code stle

--- app/controllers/bench/facilitates_controller.rb
+++ app/controllers/bench/facilitates_controller.rb
@@ -39,7 +39,12 @@ module Bench
 
     def set_cart
       if current_user
-        @cart = Trade::Cart.get_cart(params, good_type: 'Bench::Facilitate', user_id: current_user.id, **default_form_params)
+        @cart = Trade::Cart.get_cart(
+          params,
+          good_type: 'Bench::Facilitate',
+          user_id: current_user.id,
+          **default_form_params
+        )
       end
     end
 
